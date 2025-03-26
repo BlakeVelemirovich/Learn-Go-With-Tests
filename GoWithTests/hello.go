713b8b97c5243b691ce4416abefa3d6b3a3e7ee4
Blake Velemirovich <Blake.Velemirovich@gmail.com>
2025-03-26 09:00:16 -0300

    Added additional features such as named return value, grouped constants, and extracted functionality into a different function

--- GoWithTests/hello.go
+++ GoWithTests/hello.go
@@ -2,29 +2,33 @@ package main
 
 import "fmt"
 
-const spanish = "Spanish"
-const french = "French"
-const englishHelloPrefix = "Hello, "
-const spanishHelloPrefix = "Hola, "
-const frenchHelloPrefix = "Salut, "
+const (
+	spanish = "Spanish"
+	french  = "French"
+
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix  = "Salut, "
+)
 
 func Hello(name string, language string) string {
-  if (name == "") {
-    name = "world"
-  }
+	if name == "" {
+		name = "world"
+	}
 
-  prefix := englishHelloPrefix
+	return greetingPrefix(language) + name
+}
 
-  switch language {
-  case spanish:
-    prefix = spanishHelloPrefix
-
-  case french:
-    prefix = frenchHelloPrefix
-  }
-
-  return prefix + name
-
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
 }
 
 func main() {
